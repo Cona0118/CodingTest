95a4c7e34657f637807c2d097ba374dfdf520a30
Cona0118 <127915993+Cona0118@users.noreply.github.com>
2025-09-09 14:45:39 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.25 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    let newArr = s.split(" ");
+    let maxIdx = 0;
+    let minIdx = 0;
+    
+    for(let i=1; i<newArr.length; i++){
+        if(parseInt(newArr[i]) > parseInt(newArr[maxIdx])) maxIdx = i;
+        else if(parseInt(newArr[i]) < parseInt(newArr[minIdx])) minIdx = i;
+    }
+    
+    return newArr[minIdx] + " " + newArr[maxIdx];
+}
